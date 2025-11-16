826fd6b112605837bbd998b80d19930ee06f3541
Christan Toreres <torereschristan@gmail.com>
2025-11-17 00:31:40 +0800

    feat: added function that updates timer

---
+++ stopwatch/index.js
@@ -0,0 +1,28 @@
+const display = document.getElementById("display");
+const startBtn = document.getElementById("start-button");
+const stopBtn = document.getElementById("stop-button");
+const resetBtn = document.getElementById("reset-button");
+
+let timer = null;
+let startTime = 0;
+let elapsedTime = 0;
+let isRunning = false;
+
+function update(){
+    const currentTime = Date.now();
+    elapsedTime = currentTime - startTime;
+
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+    let seconds = Math.floor(elapsedTime / 1000 % 60);
+    let miliseconds = Math.floor(elapsedTime % 1000 / 10);
+
+    hours = String(hours).padStart(2, "0");
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+    miliseconds = String(miliseconds).padStart(2, "0");
+
+    display.textContent = `${hours}:${minutes}:${seconds}:${miliseconds}`;
+}
+
+
