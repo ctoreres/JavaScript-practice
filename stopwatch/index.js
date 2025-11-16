22e23f6b0c76ed823169c0a0a1fc166d09af58af
Christan Toreres <torereschristan@gmail.com>
2025-11-17 00:32:38 +0800

    feat: added buttons functions

--- stopwatch/index.js
+++ stopwatch/index.js
@@ -25,4 +25,26 @@ function update(){
     display.textContent = `${hours}:${minutes}:${seconds}:${miliseconds}`;
 }
 
+function start(){
+    if (!isRunning){
+        startTime = Date.now() - elapsedTime;
+        timer = setInterval(update, 10);
+        isRunning = true;
+    }
+}
 
+function stop(){
+    if (isRunning){
+        clearInterval(timer);
+        elapsedTime = Date.now() - startTime;
+        isRunning = false;
+    }
+}
+
+function reset(){
+    clearInterval(timer);
+    startTime = 0;
+    elapsedTime = 0;
+    isRunning = false;
+    display.textContent = "00:00:00:00"
+}
