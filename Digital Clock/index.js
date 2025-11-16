b44246956955d4c5dcbd3f7eb3f51d726ea731fa
Christan Toreres <torereschristan@gmail.com>
2025-11-16 23:11:10 +0800

    feat: added meridiem

--- Digital Clock/index.js
+++ Digital Clock/index.js
@@ -2,10 +2,13 @@ function updateClock(){
     const now = new Date();
 
     let hours = now.getHours().toString().padStart(2, 0);
+    const meridiem = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12 || 12;
+
     let minutes = now.getMinutes().toString().padStart(2, 0);
     let seconds = now.getSeconds().toString().padStart(2, 0);
 
-    const timeString = `${hours}:${minutes}:${seconds}`;
+    const timeString = `${hours}:${minutes}:${seconds} ${meridiem}`;
     
     document.getElementById("clock").textContent = timeString;
 }
