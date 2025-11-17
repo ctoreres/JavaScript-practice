2f17d29ca732aba0d256251391b16bfc39a6b488
Christan Toreres <torereschristan@gmail.com>
2025-11-17 21:30:24 +0800

    feat: added function to display values

--- calculator/index.js
+++ calculator/index.js
@@ -1,5 +1,13 @@
  const display = document.getElementById("display");
  
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function calculate(){
+
+}
+
  function clearDisplay(){
     display.textContent = ""
  }
