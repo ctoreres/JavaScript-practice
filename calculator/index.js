51dd93acf67b8086be4459af676d12369c2eecc1
Christan Toreres <torereschristan@gmail.com>
2025-11-17 21:38:19 +0800

    feat: added proper error handling

--- calculator/index.js
+++ calculator/index.js
@@ -9,5 +9,10 @@ function clearDisplay(){
 }
 
 function calculate(){
-    display.value = eval(display.value);
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Syntax Error";
+    }
 }
