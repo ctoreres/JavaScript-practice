898eb0ca23fd4f45406ca5b9d2ac8909d978e2a1
Christan Toreres <torereschristan@gmail.com>
2025-11-20 01:12:09 +0800

    feat: added main event listener

--- weather-app/index.js
+++ weather-app/index.js
@@ -1,9 +1,41 @@
-async function getWeather(){
-    const response = await fetch("");
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".cityInput");
+const card = document.querySelector(".card");
+
+// This key is currently diabled
+const apiKey = "c4cef0331104bb67e6a89785c9f41e6c";
+
+weatherForm.addEventListener("submit", async (event) => {
+    
+    event.preventDefault();
+
+    const city = cityInput.value;
+
+    if(city){
+        try{
+            const weatherData = await getWeatherData(city);
+            displayWeatherInfo(weatherData);
+        }
+        catch(error){
+            console.error(error);
+            displayError(error);
+        }
+    }
+    else{
+        displayError("Please Enter a City");
+    }
+});
+
+
+async function getWeatherData(city){
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    const response = await fetch(apiUrl);
 
     if (!response.ok){
-        throw new Error("Could not find the response");
+        throw new Error("Could not fetch weather data");
     }
 
-    const data = response.json();
+    return await response.json();
 }
+
+
