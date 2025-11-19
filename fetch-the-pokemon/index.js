4b609efe4687766c2194c05b8d4ebe9b319680ef
Christan Toreres <torereschristan@gmail.com>
2025-11-19 23:16:55 +0800

    chore: fetch the API link and setup the error handling

---
+++ fetch-the-pokemon/index.js
@@ -0,0 +1,17 @@
+async function fetchData(){
+    try{
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
+        if(!response.ok){
+            throw new Error("Could not fetch resource");
+        }
+        
+        const data = await response.json();
+        
+    }
+    catch(error){
+        console.error(error);
+    }
+}
