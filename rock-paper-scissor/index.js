8878ea38754fbf570935c4d19d96e7f5cdef6ace
Christan Toreres <torereschristan@gmail.com>
2025-11-19 17:51:33 +0800

    feat: initialize constants

---
+++ rock-paper-scissor/index.js
@@ -0,0 +1,6 @@
+const choices = ["rock", "paper", "scissor"];
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("result");
+
+
