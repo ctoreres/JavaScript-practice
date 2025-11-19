c8df05078c065ee1587a0935b43063380dba1ccd
Christan Toreres <torereschristan@gmail.com>
2025-11-19 17:52:18 +0800

    feat: added playGame function

--- rock-paper-scissor/index.js
+++ rock-paper-scissor/index.js
@@ -4,3 +4,35 @@ const computerDisplay = document.getElementById("computerDisplay");
 const resultDisplay = document.getElementById("result");
 
 
+function playGame(playerChoice){
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    let result = "";
+
+    computerDisplay.textContent = `PLAYER: ${computerChoice}`;
+    playerDisplay.textContent = `Computer: ${playerChoice}`;
+
+    if (playerChoice === computerChoice){
+        result = "IT'S A TIE!";
+    }
+    else{
+        switch(playerChoice){
+            case "rock":
+                result = (computerChoice === "scissor") 
+                ? "YOU WIN!"
+                : "YOU LOSE!";
+                break;
+            case "scissor":
+                result = (computerChoice === "paper") 
+                ? "YOU WIN!"
+                : "YOU LOSE!";
+                break;
+            case "paper":
+                result = (computerChoice === "rock") 
+                ? "YOU WIN!"
+                : "YOU LOSE!";
+                break;
+        }
+    }
+
+    resultDisplay.textContent = result;
+}
