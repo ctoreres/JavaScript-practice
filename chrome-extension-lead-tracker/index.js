be343285326d80a0f94a0931587f8b8dbd90772e
Christan Toreres <torereschristan@gmail.com>
2025-11-20 23:27:18 +0800

    feat: added event listener for save input btn

---
+++ chrome-extension-lead-tracker/index.js
@@ -0,0 +1,12 @@
+const inputBtn = document.getElementById("input-btn");
+const inputEl = document.getElementById("input-el");
+const ulEl = document.getElementById("ul-el");
+let myLeads = [];
+
+inputBtn.addEventListener("click", (event) => {
+    myLeads.push(inputEl.value);
+
+    displayLead();
+    inputEl.value = "";
+});
+
