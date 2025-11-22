ef3ea1c6071fae78944f53974aefd7de79dcbb96
Christan Toreres <torereschristan@gmail.com>
2025-11-23 00:08:50 +0800

    feat: add local storage

--- chrome-extension-lead-tracker/index.js
+++ chrome-extension-lead-tracker/index.js
@@ -1,10 +1,19 @@
 const inputBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
+
 let myLeads = [];
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+if (leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage;
+    displayLeads();
+}
 
 inputBtn.addEventListener("click", (event) => {
     myLeads.push(inputEl.value);
+
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
 
     displayLeads();
     inputEl.value = "";
